35365f314610c0b50a81213edd6adc8541086019
Swapnil Dukhande <swapnil.dukhande@move.com>
2022-03-09 23:56:30 -0700

    Amplify demo app

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "amplifyapp573ec661": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "api": {
+        "amplifyapp": {
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,103 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react';
+import Amplify, { API, graphqlOperation } from 'aws-amplify';
+import { createTodo, deleteTodo } from './graphql/mutations';
+import { listTodos as listNotes } from './graphql/queries';
+// import { listNotes } from './graphql/queries';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import awsExports from './aws-exports';
+
+Amplify.configure(awsExports);
+
+class AddNote extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: '' };
+  }
+
+  handleChange = (event) => {
+    this.setState({ text: event.target.value });
+  }
+
+  handleClick = () => {
+    this.props.addNote(this.state);
+    this.setState({ text: '' });
+  }
+
+  render() {
+    return (
+      <div style={styles.form}>
+        <input
+          value={this.state.text}
+          onChange={this.handleChange}
+          placeholder="New Note"
+          style={styles.input}
+        />
+        <button onClick={this.handleClick} style={styles.addButton}>Add Note</button>
+      </div>
+    );
+  }
 }
 
-export default App;
+class NotesList extends Component {
+  render() {
+    return (
+      <div>
+        {this.props.notes.map(note =>
+          <div key={note.id} style={styles.note}>
+            <p>{note.text}</p>
+            <button onClick={() => { this.props.deleteTodo(note) }} style={styles.deleteButton}>x</button>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { notes: [] };
+  }
+
+  async componentDidMount() {
+    var result = await API.graphql(graphqlOperation(listNotes));
+    this.setState({ notes: result.data.listNotes.items });
+  }
+
+  deleteTodo = async (note) => {
+    const id = {
+      id: note.id
+    };
+    await API.graphql(graphqlOperation(deleteTodo, { input: id }));
+    this.setState({ notes: this.state.notes.filter(item => item.id !== note.id) });
+  }
+
+  addNote = async (note) => {
+    var result = await API.graphql(graphqlOperation(createTodo, { input: note }));
+    this.state.notes.push(result.data.createTodo);
+    this.setState({ notes: this.state.notes });
+  }
+
+  render() {
+    return (
+      <div style={styles.container}>
+        <h1>Notes App</h1>
+        <AddNote addNote={this.addNote} />
+        <NotesList notes={this.state.notes} deleteTodo={this.deleteTodo} />
+        <AmplifySignOut />
+      </div>
+    );
+  }
+}
+
+export default withAuthenticator(App);
+
+const styles = {
+  container: { width: 480, margin: '0 auto', padding: 20 },
+  form: { display: 'flex', marginBottom: 15 },
+  input: { flexGrow: 2, border: 'none', backgroundColor: '#ddd', padding: 12, fontSize: 18 },
+  addButton: { backgroundColor: 'black', color: 'white', outline: 'none', padding: 12, fontSize: 18 },
+  note: { display: 'flex', justifyContent: 'space-between', alignItems: 'center', fontSize: 22, marginBottom: 15 },
+  deleteButton: { fontSize: 18, fontWeight: 'bold' }
+}
